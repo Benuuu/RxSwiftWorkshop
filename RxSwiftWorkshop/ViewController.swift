e3ac2bfe5b6c31ee203d1b6587374289a4340beb
Ben Wu <ben@intrepid.io>
2016-09-26 18:51:41 -0400

    Basic username / password validation

--- RxSwiftWorkshop/ViewController.swift
+++ RxSwiftWorkshop/ViewController.swift
@@ -14,22 +14,22 @@ class ViewController: UIViewController {
 
     let disposeBag = DisposeBag()
 
-    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var button: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let textFieldNonEmptyObservable = textField.rx.textInput.text.map { $0.characters.count > 0 }
-        textFieldNonEmptyObservable.subscribe(onNext: { (notEmpty) in
-            print(notEmpty)
-            }).addDisposableTo(disposeBag)
+        let usernameValid = usernameTextField.rx.textInput.text.map { $0.characters.count >= 3 }
+        let passwordValid = passwordTextField.rx.textInput.text.map { $0.characters.count >= 4 }
 
-        textFieldNonEmptyObservable.bindTo(button.rx.enabled).addDisposableTo(disposeBag)
+        let isValid = Observable.combineLatest(usernameValid, passwordValid) { $0 && $1 }
+
+        isValid.bindTo(button.rx.enabled).addDisposableTo(disposeBag)
     }
 
     @IBAction func buttonTapped(_ sender: AnyObject) {
         print("the user tapped the button!")
     }
 }
-
