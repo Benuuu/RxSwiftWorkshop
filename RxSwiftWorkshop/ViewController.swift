45100ac0a2b548d6d28993a08d5f302805bce051
Ben Wu <ben@intrepid.io>
2016-09-26 18:29:21 -0400

    Zip

--- RxSwiftWorkshop/ViewController.swift
+++ RxSwiftWorkshop/ViewController.swift
@@ -23,16 +23,17 @@ class ViewController: UIViewController {
         let var1 = Variable<Int>(0)
         let var2 = Variable<Int>(1)
 
-        let combined = Observable.combineLatest(var1.asObservable(), var2.asObservable()) { (v1, v2) -> String in
+        let zipped = Observable.zip(var1.asObservable(), var2.asObservable()) { (v1, v2) -> String in
             return "the pair: \(v1), \(v2)"
         }
-        let observableToPrint = combined
+        let observableToPrint = zipped
         observableToPrint.subscribe(onNext: { i in
             print(i)
         }).addDisposableTo(disposeBag)
 
 
         var1.value = 2
+        var1.value = 0
         var2.value = 3
         var1.value = 4
         var2.value = 5
