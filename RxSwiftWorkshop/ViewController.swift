a16f8493d810a15628816cd795853ea55e0a1d09
Ben Wu <ben@intrepid.io>
2016-09-26 18:45:29 -0400

    Disable the button when the textField is blank

--- RxSwiftWorkshop/ViewController.swift
+++ RxSwiftWorkshop/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     let disposeBag = DisposeBag()
 
     @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var button: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,7 +24,12 @@ class ViewController: UIViewController {
         textFieldNonEmptyObservable.subscribe(onNext: { (notEmpty) in
             print(notEmpty)
             }).addDisposableTo(disposeBag)
+
+        textFieldNonEmptyObservable.bindTo(button.rx.enabled).addDisposableTo(disposeBag)
     }
 
+    @IBAction func buttonTapped(_ sender: AnyObject) {
+        print("the user tapped the button!")
+    }
 }
 
