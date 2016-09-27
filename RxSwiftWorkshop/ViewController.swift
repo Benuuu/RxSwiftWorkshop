3a0622ed33aba5d45b81ae5bb1aff50e802b972e
Ben Wu <ben@intrepid.io>
2016-09-26 20:03:55 -0400

    Write custom rx extension

--- RxSwiftWorkshop/ViewController.swift
+++ RxSwiftWorkshop/ViewController.swift
@@ -28,6 +28,8 @@ class ViewController: UIViewController {
         viewModel.password <- passwordTextField.rx.textInput.text >>> disposeBag
 
         button.rx.enabled <- viewModel.buttonEnabled >>> disposeBag
+
+        view.rx.backgroundColor <- viewModel.backgroundColor >>> disposeBag
     }
 
     @IBAction func buttonTapped(_ sender: AnyObject) {
@@ -40,13 +42,21 @@ class LoginViewModel {
     let username = Variable<String>("")
     let password = Variable<String>("")
     let buttonEnabled: Observable<Bool>
+    let backgroundColor: Observable<UIColor?>
 
     init() {
         let usernameValid = username.asObservable().map { $0.characters.count >= 3 }
         let passwordValid = password.asObservable().map { $0.characters.count >= 4 }
 
         buttonEnabled = Observable.combineLatest(usernameValid, passwordValid) { $0 && $1 }
+        backgroundColor = buttonEnabled.map { $0 ? UIColor.green : UIColor.red }
     }
+}
 
-
+extension Reactive where Base: UIView {
+    var backgroundColor: AnyObserver<UIColor?> {
+        return UIBindingObserver(UIElement: self.base, binding: { (view, color) in
+            view.backgroundColor = color
+        }).asObserver()
+    }
 }
